ffe957b3a0c6510935f8e5c65ab8193ba2178954
mimaraslan <mimaraslan@gmail.com>
2021-02-01 14:17:17 +0300

    Concurrency Multi-threaded
    Concurrency Multi-threaded

---
+++ src/main/java/com/mimaraslan/_001_/AppMain.java
@@ -0,0 +1,7 @@
+package com.mimaraslan._001_;
+
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("Concurency 1");
+    }
+}
---
+++ src/main/java/com/mimaraslan/_002_/AppMain.java
@@ -0,0 +1,7 @@
+package com.mimaraslan._002_;
+
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("Concurency 2");
+    }
+}
