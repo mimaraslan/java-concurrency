1286449a04a545875dc2aaefefd8c2f69bff7695
mimaraslan <mimaraslan@gmail.com>
2022-06-17 00:10:04 +0300

    Concurrency Multi-threaded
    Concurrency Multi-threaded

--- src/main/java/com/mimaraslan/java17/MyClass.java
+++ src/main/java/com/mimaraslan/java17/MyClass.java
@@ -1,2 +1,18 @@
-package com.mimaraslan.java17;public class MyClass {
+package com.mimaraslan.java17;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        String a = "1" + 2;
+        String b = String.valueOf(1) + 2;
+
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+
+        String c = "12";
+        String d = String.valueOf(12);
+      //  System.out.println(c == d);
+
+    }
 }
--- src/main/java/com/mimaraslan/java17/MyClass2.java
+++ src/main/java/com/mimaraslan/java17/MyClass2.java
@@ -1,18 +1,18 @@
 package com.mimaraslan.java17;
 
-public class MyClass {
+public class MyClass2 {
 
     public static void main(String[] args) {
 
-        String a = "1" + 2;
-        String b = String.valueOf(1) + 2;
+     //   int a = 1 + 2;
+    //    int b = a++ + 2;
+    //    System.out.println(a + " " + b);
 
-        System.out.println(a == b);
-        System.out.println(a.equals(b));
 
-        String c = "12";
-        String d = String.valueOf(12);
-      //  System.out.println(c == d);
+        boolean a = false, b = false;
+        boolean c = b = a = !a;
+        System.out.println(a + " " + b + " " + c);
+
 
     }
 }
